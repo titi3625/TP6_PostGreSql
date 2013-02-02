4e63b3706a7a356e2b5f0de95ceb3aa7e5c1fe97
Coco <coco@coco-VirtualBox.(none)>
2013-02-02 23:31:55 +0100

    salut

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php 
+require("core.php");
+?>
+<!DOCTYPE HTML>
+<html lang="fr-FR">
+<head>
+	<meta charset="UTF-8">
+	<title>TP6</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>TP6 SLAM3</h1>
+	<p>bonjour</p>
 
-include_once('modele/connexion_bdd.php');
 
-include_once('controleur.index.php');
+
+	<?php
+		$category = Model::load("category");
+		$category->id = 2;
+		$category->read();
+		echo $category;
+	?>
+</body>
+</html>
